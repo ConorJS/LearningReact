d86e66fb1cd69790668d3db7078a8ead0aedcd82
Conor <aeimo@protonmail.com>
2020-05-06 21:45:17 +1000

    Have Game component respond to click events. Got player alternating working.

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -2,89 +2,92 @@
 
 import React from 'react';
 import Square from './Square';
-  
+
 //== class init =======================================================================================================
 
 export default class Board extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        // Squares should just contain a reference to the player that 'owns' the square, or null if none do.
-        squares: Array(this.props.boardSize * this.props.boardSize).fill(null)
-      };
-    }
+  constructor(props) {
+    super(props);
 
-    //== render =======================================================================================================
-  
-    render() {
-      let rowsInTable = [];
-  
-      for (let i = 0; i < this.props.boardSize; i++) {
-        rowsInTable.push(this.renderRow(i));
-      }
-  
-      return (
-        <div>{rowsInTable}</div>
-      );
+    this.state = {
+      // Squares should just contain a reference to the player that 'owns' the square, or null if none do.
+      squares: Array(this.props.boardSize * this.props.boardSize).fill(null)
     };
-  
-    /**
-     * Renders a whole row, using {@link renderSquare}.
-     * 
-     * @param {*} rowNumber The row number to render (0 is the top row).
-     */
-    renderRow(rowNumber) {
-      const squaresInRow = [];
-  
-      for (let i = 0; i < this.props.boardSize; i++) {
-        squaresInRow.push(this.renderSquare(i, rowNumber, this.getSquare(i, rowNumber)));
-      }
-  
-      return <div key={`row${rowNumber}`} className="board-row">{squaresInRow}</div>;
-    };
-  
-    /**
-     * Renders a single square.
-     * 
-     * @param {*} x The x-coordinate of the square in the game matrix.
-     * @param {*} y The y-coordinate of the square in the game matrix.
-     * @param {*} playerMarker The marker of the player that 'owns' the square (can be null if none does).
-     */
-    renderSquare(x, y, playerMarker) {
-      return <Square
-        key={`square_x_${x}_y_${y}`}
-        clickHandler={() => this.squareClickHandler(x, y)}
-        playerMarker={playerMarker}>
-      </Square>;
-    };
-
-    //== helpers ======================================================================================================
-    
-    componentDidCatch(error, info) {
-      console.log(`In componentDidCatch ERROR: ${error}, INFO: ${info}`);
-      this.setState({hasError: true});
-    };
-  
-    squareClickHandler(x, y) {
-      console.log(`Clicked x=${x}, y=${y}`);
-  
-      this.setSquare(x, y, this.props.playerWhosTurnItIs.marker);
-    }
-  
-    getSquare(x, y) {
-      let indexOfSquare = (y * this.boardSize) + x;
-      return this.state.squares[indexOfSquare];
-    }
-  
-    setSquare(x, y, playerMarker) {
-      let indexOfSquare = (y * this.boardSize) + x;
-  
-      let sqauresTemp = this.state.squares.slice();
-      sqauresTemp[indexOfSquare] = playerMarker;
-  
-      this.setState({
-        squares: sqauresTemp
-      });
-    }
   }
+
+  //== render =========================================================================================================
+
+  render() {
+    let rowsInTable = [];
+
+    for (let i = 0; i < this.props.boardSize; i++) {
+      rowsInTable.push(this.renderRow(i));
+    }
+
+    return (
+      <div>{rowsInTable}</div>
+    );
+  };
+
+  /**
+   * Renders a whole row, using {@link renderSquare}.
+   * 
+   * @param {*} rowNumber The row number to render (0 is the top row).
+   */
+  renderRow(rowNumber) {
+    const squaresInRow = [];
+
+    for (let i = 0; i < this.props.boardSize; i++) {
+      squaresInRow.push(this.renderSquare(i, rowNumber, this.getSquare(i, rowNumber)));
+    }
+
+    return <div key={`row${rowNumber}`} className="board-row">{squaresInRow}</div>;
+  };
+
+  /**
+   * Renders a single square.
+   * 
+   * @param {*} x The x-coordinate of the square in the game matrix.
+   * @param {*} y The y-coordinate of the square in the game matrix.
+   * @param {*} playerMarker The marker of the player that 'owns' the square (can be null if none does).
+   */
+  renderSquare(x, y, playerMarker) {
+    return <Square
+      key={`square_x_${x}_y_${y}`}
+      clickHandler={() => this.squareClickHandler(x, y)}
+      playerMarker={playerMarker}>
+    </Square>;
+  };
+
+  //== helpers ========================================================================================================
+
+  componentDidCatch(error, info) {
+    console.log(`In componentDidCatch ERROR: ${error}, INFO: ${info}`);
+    this.setState({ hasError: true });
+  };
+
+  squareClickHandler(x, y) {
+    console.log(`Clicked x=${x}, y=${y}`);
+
+    this.setSquare(x, y, this.props.playerWhosTurnItIs.marker);
+
+    this.props.squareClickHandler();
+  }
+
+  getSquare(x, y) {
+    let indexOfSquare = (y * this.props.boardSize) + x;
+    return this.state.squares[indexOfSquare];
+  }
+
+  setSquare(x, y, playerMarker) {
+    console.log(`Entered setSquare; clicked x=${x}, y=${y}. Applying player marker ${playerMarker}.`);
+    let indexOfSquare = (y * this.props.boardSize) + x;
+
+    let sqauresTemp = this.state.squares.slice();
+    sqauresTemp[indexOfSquare] = playerMarker;
+
+    this.setState({
+      squares: sqauresTemp
+    });
+  }
+}
--- tic-tac-toe/src/Game.js
+++ tic-tac-toe/src/Game.js
@@ -9,128 +9,128 @@ const BOARD_SIZE = 3;
 
 const PLAYER_INFO = {
   PLAYER_ONE: {
-    marker: 'x' 
+    marker: 'X'
   },
   PLAYER_TWO: {
-    marker: 'O' 
+    marker: 'O'
   }
 };
 
 //== class init =======================================================================================================
 
 export default class Game extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        hasError: false,
-        errorMessage: '',
-  
-        playerWhosTurnItIs: PLAYER_INFO.PLAYER_TWO
-      };
-  
-      this.boardSize = BOARD_SIZE;
-  
-      this.moves = [];
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+      errorMessage: '',
+
+      playerWhosTurnItIs: PLAYER_INFO.PLAYER_TWO
     };
 
-    //== render ======================================================================================================
-  
-    render() {
-      if (this.props.hasError) {
-        return (
-          <div>Error: {!this.props.errorMessage ? "<No message>" : this.props.errorMessage}</div>
-        );
-  
-      } else {
-        return (
-          <div className="game">
-            <div className="game-board">
-              <Board 
-                boardSize={this.boardSize} 
-                playerWhosTurnItIs={this.state.playerWhosTurnItIs}
-                squareClickHandler={this.squareClickHandler}
-              />
-            </div>
-            <div className="game-info">
-              <div className="status">Next player: {this.state.playerWhosTurnItIs.marker}</div>
-              <div className="move-history-list">
-                {this.renderMoveHistoryList()}
-              </div>
+    this.boardSize = BOARD_SIZE;
+
+    this.moves = [];
+  };
+
+  //== render =========================================================================================================
+
+  render() {
+    if (this.props.hasError) {
+      return (
+        <div>Error: {!this.props.errorMessage ? "<No message>" : this.props.errorMessage}</div>
+      );
+
+    } else {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board
+              boardSize={this.boardSize}
+              playerWhosTurnItIs={this.state.playerWhosTurnItIs}
+              squareClickHandler={() => this.onTurn()}
+            />
+          </div>
+          <div className="game-info">
+            <div className="status">Next player: {this.state.playerWhosTurnItIs.marker}</div>
+            <div className="move-history-list">
+              {this.renderMoveHistoryList()}
             </div>
           </div>
-        );
-      }
-    };
-  
-    renderMoveHistoryList() {
-      const moveHistoryList = [];
-  
-      for (let i = 0; i < this.moves.length; i++) {
-        moveHistoryList.push(this.renderMoveHistoryItem(this.moves[i]));
-      }
-  
-      return moveHistoryList;
-    };
-  
-    renderMoveHistoryItem(moveNumber) {
-      const moveDescription = moveNumber === 0 ? "game start" : ("move #" + moveNumber);
-  
-      return (
-        <div className="move-history-item">
-          <span>{moveNumber + 1}: </span>
-          <button>Go to {moveDescription}</button>
         </div>
       );
-    };
+    }
+  };
 
-    //== helpers ======================================================================================================
-  
-    createMove(x, y) {
-      if (x < 0 || x >= this.boardSize || y < 0 || y >= this.boardSize) {
-        this.makeError(`Co-ordinates [${x},${y}] fall outside of a ${this.boardSize}-sized board.`);
-        return undefined;
-      }
-  
-      return { "x": x, "y": y };
-    };
-  
-    makeError(message) {
-      this.setState({
-        hasError: true,
-        errorMessage: message
-      });
-    };
-  
-    onTurn() {
-      this.updatePlayerWhosTurnItIs();
-  
-      // TODO Check victory conditions on board
+  renderMoveHistoryList() {
+    const moveHistoryList = [];
+
+    for (let i = 0; i < this.moves.length; i++) {
+      moveHistoryList.push(this.renderMoveHistoryItem(this.moves[i]));
     }
-  
-    /**
-     * Updates the player turn state (from the player whos turn it is now, to the next player)
-     */
-    updatePlayerWhosTurnItIs() {
-      this.setState({
-        playerWhosTurnItIs: this.nextPlayer(this.state.playerWhosTurnItIs)
-      });
+
+    return moveHistoryList;
+  };
+
+  renderMoveHistoryItem(moveNumber) {
+    const moveDescription = moveNumber === 0 ? "game start" : ("move #" + moveNumber);
+
+    return (
+      <div className="move-history-item">
+        <span>{moveNumber + 1}: </span>
+        <button>Go to {moveDescription}</button>
+      </div>
+    );
+  };
+
+  //== helpers ========================================================================================================
+
+  createMove(x, y) {
+    if (x < 0 || x >= this.boardSize || y < 0 || y >= this.boardSize) {
+      this.makeError(`Co-ordinates [${x},${y}] fall outside of a ${this.boardSize}-sized board.`);
+      return undefined;
     }
-  
-    /**
-     * Determines who's turn it is next.
-     * 
-     * @param {*} currentPlayer The current player.
-     */
-    nextPlayer(currentPlayer) {
-      if (currentPlayer === PLAYER_INFO.PLAYER_ONE) {
-        return PLAYER_INFO.PLAYER_TWO;
-        
-      } else if (currentPlayer === PLAYER_INFO.PLAYER_TWO) {
-        return PLAYER_INFO.PLAYER_ONE;
-  
-      } else {
-        return undefined;
-      }
+
+    return { "x": x, "y": y };
+  };
+
+  makeError(message) {
+    this.setState({
+      hasError: true,
+      errorMessage: message
+    });
+  };
+
+  onTurn() {
+    this.updatePlayerWhosTurnItIs();
+
+    // TODO Check victory conditions on board
+  }
+
+  /**
+   * Updates the player turn state (from the player whos turn it is now, to the next player)
+   */
+  updatePlayerWhosTurnItIs() {
+    this.setState({
+      playerWhosTurnItIs: this.nextPlayer(this.state.playerWhosTurnItIs)
+    });
+  }
+
+  /**
+   * Determines who's turn it is next.
+   * 
+   * @param {*} currentPlayer The current player.
+   */
+  nextPlayer(currentPlayer) {
+    if (currentPlayer === PLAYER_INFO.PLAYER_ONE) {
+      return PLAYER_INFO.PLAYER_TWO;
+
+    } else if (currentPlayer === PLAYER_INFO.PLAYER_TWO) {
+      return PLAYER_INFO.PLAYER_ONE;
+
+    } else {
+      return undefined;
     }
   }
+}
